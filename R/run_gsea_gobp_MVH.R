c7a4fa1784c106e15ea7aa3feb66e99c430e402a
mvhunter1 <hunterm@mskcc.org>
2021-07-20 11:51:50 -0400

    adding new GSEA functions

--- R/run_gsea_gobp_MVH.R
+++ R/run_gsea_gobp_MVH.R
@@ -12,7 +12,7 @@ run_gsea_gobp_MVH <- function(marker_list, filter_pval = T) {
   require(mvhfunctions)
   
   # check for msigdbr loaded already
-  
+  message('Loading pathways...\n')
   human.genes <- msigdbr(species = "Homo sapiens")
   genesets.GOBP <- filter(human.genes, gs_subcat == "GO:BP") %>% split(x = .$gene_symbol, f = .$gs_name)
   
@@ -21,16 +21,21 @@ run_gsea_gobp_MVH <- function(marker_list, filter_pval = T) {
     marker_list <- marker_list %>% filter(p_val_adj <= 0.05)
   }
   
+  message('Converting genes to human...\n')
   # convert to human
   marker_list_human <- marker_list %>% convert_FindMarkers_human_MVH()
   
+  message('Running fGSEA...\n')
+  
   # run gsea
+  set.seed(3)
   gsea_gobp <- fgseaMultilevel(pathways = genesets.GOBP,
                                stats = marker_list_human) %>%
     arrange(desc(NES)) %>% 
     mutate(log10pval = -log10(pval)) %>%
     select(pathway, NES, pval, log10pval, everything())
   
+  message('Done!\n')
   return(gsea_gobp)
  
 }
