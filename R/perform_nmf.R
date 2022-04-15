bc82cc254b54db5de57c4fb9ef9849526f3ea645
mvhunter1 <hunterm@mskcc.org>
2022-04-15 13:58:19 -0400

    edits to perform_nmf

--- R/perform_nmf.R
+++ R/perform_nmf.R
@@ -1,20 +1,16 @@
 #' @title perform_nmf
-#' @description perform NMF on a matrix of expression data.
-#' @param expression_matrix For scRNA-seq or Visium, typically the output from GetAssayData for your cluster/cell type of interest.
-#' @param rank Rank used for NMF, can be determined using NMFestimaterank.
-#' @param save_NMF_results if T, will save NMF function output as .R file. This can be useful since the function takes several hours to run.
-#' @param filename if save_NMF_results = T, filename that the NMF results will be saved as.
+#' @description Perform NMF and create gene modules from expression matrix. Slightly modified code from Reuben and Dalia.
+#' @param expression_matrix Expression matrix, commonly variable genes, for cell type of interest.
+#' @param rank Rank for NMF, default = 20. Can be a vector of possible ranks but can only use multiple ranks if use_fast_method = F.
+#' @param save_NMF_results Save NMF results matrix in working dir?
+#' @param filename Filename to save NMF results as.
+#' @param use_fast_method If T, will use the fast NMF method implemented in RcppML. Can only be used for a single rank.
 #' @export
-#' @return a list containing the NMF results matrix, coefficients, scores, and modules.
+#' @return NMF results list with matrix, modules, scores and coef.
 
-perform_nmf <- function(expression_matrix, rank = 20, save_NMF_results = F, filename = NULL) {
+perform_nmf <- function(expression_matrix, rank = 20, save_NMF_results = F, filename = NULL, use_fast_method = F) {
   
-  # modified code from Reuben
-  
-  require(NMF)
-  require(fastICA)
-  
-  # make sure matrix is saved as matrix
+  # make sure matrix is stored as matrix
   expression_matrix <- as.matrix(expression_matrix)
   
   # check to see if there are any negative values and if so, set to 0
@@ -27,8 +23,25 @@ perform_nmf <- function(expression_matrix, rank = 20, save_NMF_results = F, file
   
   # run NMF
   message('Running NMF...')
-  nmf_results <- NMF::nmf(x = expression_matrix_filt, rank = rank, seed = 'ica', method = 'nsNMF')
+  t1 <- Sys.time() 
+  if (use_fast_method) {
+    require(RcppML) 
+    require(Matrix)
+    nmf_results <- RcppML::nmf(A = Matrix::Matrix(expression_matrix_filt, sparse = T), 
+                               k = rank, 
+                               seed = 'ica') 
+  } else {
+    require(NMF)
+    require(fastICA)
+    nmf_results <- NMF::nmf(x = expression_matrix_filt, 
+                            rank = rank, 
+                            seed = 'ica', 
+                            #.options = list(parallel=T),
+                            method = 'nsNMF')
+  }
+  t2 <- Sys.time() 
   message('NMF calculations complete!')
+  difftime(t2,t1)
   
   if (save_NMF_results) {
     message('Saving NMF results...')
@@ -41,17 +54,24 @@ perform_nmf <- function(expression_matrix, rank = 20, save_NMF_results = F, file
   
   # Make modules
   message('Making modules...')
-  scores <- basis(nmf_results)
-  coef <- coefficients(nmf_results)
+  if (use_fast_method) {
+    scores <- nmf_results$w
+    coef <- nmf_results$h
+    rownames(scores) <- rownames(expression_matrix_filt) # critical!!!!
+    colnames(coef)<- colnames(expression_matrix_filt) # critical!!!!
+  } else {
+    scores <- basis(nmf_results)
+    coef <- coefficients(nmf_results)
+  }
   
   o <- order(apply(scores, 2, var), decreasing = TRUE)
   scores <- scores[, o]
   coef <- coef[o, ]
   
-  # Remove if fewer than 5 genes
+  # Make modules
   scores.norm <- t(scores) / apply(scores, 2, mean)
-  ranks_x <- t(apply(-t(scores.norm), 1, rank))
-  ranks_y <- apply(-t(scores.norm), 2, rank)
+  ranks_x <- t(apply(-t(scores.norm), 1, base::rank))
+  ranks_y <- apply(-t(scores.norm), 2, base::rank)
   
   for (i in 1:ncol(scores)){
     ranks_y[ranks_x[,i] > 1,i] = Inf
@@ -64,16 +84,17 @@ perform_nmf <- function(expression_matrix, rank = 20, save_NMF_results = F, file
   })
   
   l <- sapply(modules, length)
-  keep <- (l >= 10)
+  keep <- (l >= 5) # remove any module with less than 5 genes
   scores <- scores[, keep]
   coef <- coef[keep, ]
   
+  # Repeat module making
   ranks_x <- t(apply(-t(t(scores) / apply(scores, 2, mean)), 1, rank))
   ranks_y <- apply(-t(t(scores) / apply(scores, 2, mean)), 2, rank)
   for (i in 1:ncol(scores)){
     ranks_y[ranks_x[,i] > 1,i] = Inf
   }
-  modules <- apply(ranks_y, 2, function(m){
+  modules_fast_fxn <- apply(ranks_y, 2, function(m){
     a <- sort(m[is.finite(m)])
     a <- a[a == 1:length(a)]
     names(a)
