6eacd728e7970aa764e454b221d1786a84175b63
mvhunter1 <hunterm@mskcc.org>
2023-08-01 13:31:31 -0400

    small change to nice_spatial_feature_plot

--- R/nice_spatial_feature_plot.R
+++ R/nice_spatial_feature_plot.R
@@ -19,7 +19,7 @@
 
 nice_spatial_feature_plot <- function(seurat_obj, features, im_alpha = 0, pt_size = 1.3, 
                                         stroke = 0, new_cmap = T, cmap = "inferno", n_col = NULL, 
-                                        diverging_cmap = F, alpha = T, cols = NULL, crop = T, min.cutoff = 0, max.cutoff = NULL) 
+                                        diverging_cmap = F, alpha = T, cols = NULL, crop = T, min.cutoff = 0, max.cutoff = NA) 
 {
   
   require(Seurat)
